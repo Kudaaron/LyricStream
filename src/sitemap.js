b21ee0a2c35f49a161ed9df0bab38217688bbc99
Kudaaron <kudaaron10@gmail.com>
2026-07-08 19:21:56 +0530

    Setting xml

--- src/sitemap.js
+++ src/sitemap.js
@@ -1,9 +1,10 @@
 export default function sitemap() {
   return [
-    { url:"https://lyric-stream-six.vercel.app/", 
-      priority: 1.0, 
-      changefreq: "quarterly"
-      lastModifiedd: new Date()
+    {
+      url: "https://lyric-stream-six.vercel.app/",
+      priority: 1.0,
+      changefreq: "quarterly",
+      lastModifiedd: new Date(),
+    },
+  ];
 }
-  ]
-} 
